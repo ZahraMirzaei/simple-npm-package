bee15cab4eb0f4e33ab6959a4dc1726356f4c342
Zahra Mirzaei <z.mirzaei2019@gmail.com>
2023-05-05 11:49:19 +0330

    feat: add style to title

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import '../style.css'
 
 type Props = {
   value?: number
@@ -16,7 +17,7 @@ const MyCounter = ({ value = 0 }: Props) => {
 
   return (
     <div>
-      <h1>Counter: {counter}</h1>
+      <h1 className='title'>Counter: {counter}</h1>
       <button onClick={onMinus}>-</button>
       <button onClick={onPlus}>+</button>
     </div>
